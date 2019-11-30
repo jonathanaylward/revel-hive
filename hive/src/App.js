3dff7959219e941b1301d91f7806e2526ce6dfa6
Jonathan Aylward <jonny@Jens-MacBook-Air.local>
2019-11-30 12:00:46 -0700

    polygon

--- hive/src/App.js
+++ hive/src/App.js
@@ -9,8 +9,9 @@ function App() {
     <div className="App">
         <svg>
         <circle cx={50} cy={50} r={10} fill="red" />
-        <polygon cy={200} points="100,100 50,50, 0,0" />
-      </svg>
+        <polygon points="200,0 275,75 275,150, 200,225, 125,150 125,75" style={{fill:"lime",stroke:"purple","stroke-width":1}} />
+        Sorry, your browser does not support inline SVG.
+        </svg>
       eat my shorts
       <Box></Box>
     </div>
