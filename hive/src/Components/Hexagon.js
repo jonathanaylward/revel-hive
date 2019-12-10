52cccaa029afaf7570a3bf9889d7dc86471239bc
Jonathan Aylward <jonathan.aylward@gmail.com>
2019-12-09 21:01:24 -0700

    organized hexagons and need to make images as we will use images instead of hexagon svg

--- hive/src/Components/Hexagon.js
+++ hive/src/Components/Hexagon.js
@@ -8,26 +8,11 @@ class Hexagon extends React.Component {
         };  
       }
     render() {
-        const hFill = this.props.fill;
-        const hStroke = this.props.stroke;
-        console.log(this.props.fill);
         return (
 
             <svg height={200} width={200}
                 style={this.props.SvgAnimationStyle}            
             >
-                <defs>
-                    <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
-                        <stop offset="90%" style={{
-                            stopColor:`${hFill}`, 
-                            stopOpacity:"1",
-                        }} />
-                        <stop offset="100%" style={{
-                            stopColor:`${hStroke}`,
-                            stopOpacity:"1",
-                        }} />
-                    </linearGradient>
-                </defs>
                 <g>
                 <polygon
                     points={"0,90 50,0 150,0 200,90 150,180 50,180"}
@@ -38,7 +23,7 @@ class Hexagon extends React.Component {
                     ref={this.props.hexagonRef}
                     className={this.props.classNames}
                     style={{
-                        fill:"url(#grad1)",
+                        fill:this.props.fill,
                         stroke:`${this.props.stroke}`,
                         strokeWidth:5
                     }}
